7c612340f212d64cddc7178498cfa78118dc738c
Vishal <vishalpuppala@gmail.com>
2018-05-10 18:58:28 +0530

    removed

--- test-web/js/grid.js
+++
@@ -1,8 +0,0 @@
-// Four images side by side
-function four() {
-    var elements = document.getElementsByClassName("column");
-    var i;
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.width = "25%";
-    }
-}
